6576ff38410237a6d9a133c303e3f2d1d0092b4e
vellasofywares <harshal.mohite7707@gmail.com>
2020-03-09 15:04:09 +0530

    updated progress bar

--- app/src/main/java/com/example/libraapplication/ProgressDialogData.java
+++ app/src/main/java/com/example/libraapplication/ProgressDialogData.java
@@ -8,16 +8,16 @@ import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 
 public class ProgressDialogData extends Dialog {
-    private ProgressBar progressBar;
 
     public ProgressDialogData(@NonNull Context context) {
-        super(context,android.R.style.Theme_DeviceDefault_Light_NoActionBar_Fullscreen);
+        super(context,android.R.style.Theme_DeviceDefault_Light_Dialog_MinWidth);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.please_wait_progress_dialog);
-        progressBar = findViewById(R.id.progress_data);
+        setTitle("Please Wait..");
+        setCancelable(false);
     }
 }
